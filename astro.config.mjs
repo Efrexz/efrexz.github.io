0a2c11e939ece09b537085280b0bd2676ffb2047
Efrexz <zyzz_448@hotmail.com>
2025-04-16 09:10:10 -0400

    arreglando base del astroConfig por error de estilos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://Efrexz.github.io",
-  base: "portfolioDev",
+  base: "/",
 });
